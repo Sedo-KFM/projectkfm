fec8ae3f10e042bc3c1ef40b41df5fa903b81e15
Sedo-KFM <fedor122448@gmail.com>
2019-12-21 14:27:20 +0400

    timeline commit 3

---
+++ views/timeline/index.php
@@ -0,0 +1,7 @@
+<?php
+	$this-> title = 'My first chart';;
+?>
+<div id="timeline">
+	<h1><?= $this->title ?></h1>
+	<iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1JMjENN79T4NEqu4BAzQw7jTANLlk53nlY1DF166QRC8&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+</div>
